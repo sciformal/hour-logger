f1e477a8ad80c810cb00a3fb0656faf894e6a801
Brent Champion <brent.champion@queensu.ca>
2021-09-10 22:21:30 -0400

    refactor backend to typescript

--- api/webpack.config.js
+++ api/webpack.config.js
@@ -1,37 +1,48 @@
-const path = require("path");
-const nodeExternals = require("webpack-node-externals");
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+const slsw = require('serverless-webpack');
+const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
-    mode: "development",
-    resolve: {
-        extensions: [".mjs", ".json", ".js"],
-        symlinks: false,
-        cacheWithContext: false,
+  context: __dirname,
+  mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
+  entry: slsw.lib.entries,
+  devtool: slsw.lib.webpack.isLocal ? 'cheap-module-eval-source-map' : 'source-map',
+  resolve: {
+    extensions: ['.mjs', '.json', '.ts'],
+    symlinks: false,
+    cacheWithContext: false,
+    alias: {
+      '@src': path.resolve(__dirname, './src'),
+      '@util': path.resolve(__dirname, './src/util'),
+      '@tests': path.resolve(__dirname, './tests'),
     },
-    output: {
-        libraryTarget: "commonjs",
-        path: path.join(__dirname, ".webpack"),
-        filename: "[name].js",
-    },
-    target: "node",
-    externals: [nodeExternals()],
-    module: {
-        rules: [
-            {
-                test: /\.(tsx?)$/,
-                loader: "ts-loader",
-                exclude: [
-                    [
-                        path.resolve(__dirname, "node_modules"),
-                        path.resolve(__dirname, ".serverless"),
-                        path.resolve(__dirname, ".webpack"),
-                    ],
-                ],
-                options: {
-                    transpileOnly: true,
-                    experimentalWatchApi: true,
-                },
-            },
+  },
+  output: {
+    libraryTarget: 'commonjs',
+    path: path.join(__dirname, '.webpack'),
+    filename: '[name].js',
+  },
+  target: 'node',
+  externals: [nodeExternals()],
+  module: {
+    rules: [
+      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+      {
+        test: /\.(tsx?)$/,
+        loader: 'ts-loader',
+        exclude: [
+          [
+            path.resolve(__dirname, 'node_modules'),
+            path.resolve(__dirname, '.serverless'),
+            path.resolve(__dirname, '.webpack'),
+          ],
         ],
-    },
+        options: {
+          transpileOnly: true,
+          experimentalWatchApi: true,
+        },
+      },
+    ],
+  }
 };
