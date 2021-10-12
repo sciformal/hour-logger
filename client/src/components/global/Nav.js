8e15761b4c543f708eea6e937b6df8352865ff26
Brent Champion <brent.champion@queensu.ca>
2021-10-11 21:58:37 -0400

    update navigation bar

--- client/src/components/global/Nav.js
+++ client/src/components/global/Nav.js
@@ -1,7 +1,7 @@
 import { Auth } from "aws-amplify";
 import { useAuthenticationContext, useUserContext } from "libs/contextLib";
 import React from "react";
-import { Container, Nav, Navbar } from "react-bootstrap";
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 
 export default function HourLoggerNav() {
   const { isAuthenticated, userHasAuthenticated } = useAuthenticationContext();
@@ -17,29 +17,36 @@ export default function HourLoggerNav() {
   
     return (
       <Navbar bg="light" expand="lg">
-        <Container>
+        <Container fluid>
           <Navbar.Brand href="/">Sci Formal Hour Logger</Navbar.Brand>
+          <div className="justify-content-end">
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="/">Hours</Nav.Link>
+            <Nav>
+              <NavDropdown id="hours-dropdown" title="Hours">
+                <NavDropdown.Item href="/">My Hours</NavDropdown.Item>
+                <NavDropdown.Item href="/reduction">Hour Reduction</NavDropdown.Item>
+
+                {(type === "ADMIN" || type === "MANAGER" || type === "COMMS") && (
+                <NavDropdown.Item href="/check-in">Volunteer Check-In</NavDropdown.Item>
+                )}
+
+                {type === "ADMIN" && (
+                  <NavDropdown.Item href="/edit-hours">Edit Hours</NavDropdown.Item>
+                )}
+              </NavDropdown>
+
               <Nav.Link href="/payment">Ticket Purchase</Nav.Link>
 
-              {/* Show Volunteer Check-in for all managers */}
-              {(type === "ADMIN" || type === "MANAGER" || type === "COMMS") && (
-                <Nav.Link href="/check-in">Volunteer Check-In</Nav.Link>
-              )}
               {(type === "ADMIN" || type === "COMMS") && (
                 <Nav.Link href="/reduction-request">
                   Reduction Requests
                 </Nav.Link>
               )}
-              {type === "ADMIN" && (
-                <Nav.Link href="/edit-hours">Edit Hours</Nav.Link>
-              )}
               <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
             </Nav>
           </Navbar.Collapse>
+          </div>
         </Container>
       </Navbar>
     );
