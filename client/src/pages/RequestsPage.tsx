b327ca8b3ecff67bf2438ad3b2bd794370936269
Brent Champion <brent.champion@queensu.ca>
2022-02-13 02:11:44 -0500

    updates requests page (#116)

--- client/src/pages/RequestsPage.tsx
+++ client/src/pages/RequestsPage.tsx
@@ -1,8 +1,7 @@
 import API from '@aws-amplify/api';
 import { useEffect, useState } from 'react';
-import { Tab, Tabs } from 'react-bootstrap';
 import Loader from '../components/global/Loader';
-import RequestCard from '../components/requests/RequestCard';
+import Requests from '../components/requests/RequestTable';
 
 export const RequestsPage = () => {
   const [requests, setRequests] = useState(null);
@@ -14,60 +13,27 @@ export const RequestsPage = () => {
 
   const loadRequests = async () => {
     const requests = await API.get('hour-logger', '/requests', {});
-
-    const pendingRequests = requests.filter(
-      request => request.status === 'PENDING',
-    );
-
-    setRequests(pendingRequests);
+    setRequests(requests);
     setLoading(false);
   };
 
   if (loading) {
-    return <Loader />;
+    return (
+      <div
+        className="App"
+        style={{ height: '100vh', lineHeight: '100vh', margin: 'auto' }}
+      >
+        <Loader />
+      </div>
+    );
   } else {
     return (
       <div style={{ padding: '5%', textAlign: 'center' }}>
         <h2>Requests</h2>
         <br />
         <br />
-        <Tabs defaultActiveKey="first">
-          <Tab eventKey="first" title="Reduction Requests">
-            <ReductionRequests requests={requests} />
-          </Tab>
-          <Tab eventKey="second" title="Transfer Requests">
-            <ReductionRequests requests={requests} />
-          </Tab>
-          <Tab eventKey="third" title="All Requests">
-            <ReductionRequests requests={requests} />
-          </Tab>
-        </Tabs>
+        <Requests requests={requests} />
       </div>
     );
   }
-};
-
-const ReductionRequests = ({ requests }) => {
-  return (
-    <div style={{ textAlign: 'center' }}>
-      <h1 style={{ textAlign: 'center', padding: '50px' }}>
-        View Reduction Requests
-      </h1>
-      {/* @ts-ignore */}
-      <div
-        style={{
-          width: '70%',
-          margin: 'auto',
-          display: 'flex',
-          gap: '50px',
-          flexWrap: 'wrap',
-          justifyContent: 'center',
-        }}
-      >
-        {requests.map(request => (
-          <RequestCard request={request} />
-        ))}
-      </div>
-    </div>
-  );
 };
